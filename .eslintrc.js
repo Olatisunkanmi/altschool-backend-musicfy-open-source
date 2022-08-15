8a3dee58e6b8f0a8b5eb41274a8ef4d90f28ce3c
omotega <tomoyibo@gmail.com>
2022-08-15 09:12:45 +0100

    added linting to the project

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'linebreak-style': 0,
+    'no-console': 0,
+    'no-unused-vars': 'off',
+  },
+};
